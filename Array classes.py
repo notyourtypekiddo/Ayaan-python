8eadb5ba978f9f68e87596a3c62b9e30c8040270
Ayaan <codewithme619@gmail.com>
2024-06-11 16:21:12 +0530

    My first code using Python

---
+++ Array classes.py
@@ -0,0 +1,7 @@
+arr = [2,6,7,9]
+minElement = float('inf')
+for i in arr:
+    if i < minElement:
+        minElement = i 
+print(minElement)
+        
---
+++ hello world.py
@@ -0,0 +1,8 @@
+vovels_a = input("Enter a string: ") 
+no_of_vovels = 0
+for i in vovels_a:
+    if i == 'A'or i == 'E' or i== 'I' or i== 'O' or i=='U' or i=='a'or i =='e' or i == 'i' or i == 'o' or i == 'u':
+        no_of_vovels = 1 + no_of_vovels 
+      
+print(no_of_vovels)     
+    
